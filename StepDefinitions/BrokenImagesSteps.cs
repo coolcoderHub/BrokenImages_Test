febb43f923195fb212e70b32c690b7a668518483
coolcoderHub <coolcoderHub@users.noreply.github.com>
2022-09-04 23:57:11 +0300

    just broken img check
    just broken img check

--- StepDefinitions/BrokenImagesSteps.cs
+++ StepDefinitions/BrokenImagesSteps.cs
@@ -43,43 +43,8 @@ namespace UnitTestProject_SpecFlow.StepDefinitions
                     String linkURL = imagelist[q].GetAttribute("src");
                     Console.WriteLine("linkURL = " + linkURL);
 
-                    Uri url = new Uri(linkURL);
-
-                    try
-                    {
-                        var request = (HttpWebRequest)WebRequest.Create(url);
-                        request.Method = "GET";
-
-                        using (var response = (HttpWebResponse)request.GetResponse())
-                        {
-                            using (var stream = response.GetResponseStream())
-                            {
-                                using (var sr = new StreamReader(stream))
-                                {
-                                    var statusCod = response.StatusCode;
-                                    int statusCode = (int)statusCod;
-
-                                    Console.WriteLine(linkURL + " and its Status codes is:" + statusCode.ToString());
-                                    if (statusCode == 404 || statusCode == 500)
-                                    {
-                                        if (statusCode != 200)
-                                        {
-                                            brokenImagesCount = brokenImagesCount + 1;
-                                            Console.WriteLine(linkURL + " and its Status codes is:" + statusCode.ToString());
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        // handle err
-                    }
-
-                //Console.WriteLine("The total number of broken images are:" + brokenImagesCount);
-
-            }
+                    if (imagelist[q].GetAttribute("naturalWidth").Equals("0")) brokenImagesCount++;
+                }
 
         }
 
